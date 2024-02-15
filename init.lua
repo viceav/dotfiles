ce1cfc8fbe563b4225c32b2d65e053a01fe7e68a
viceav <vicentealamosv@gmail.com>
2024-02-15 00:48:56 -0300

    feat: add init.lua from NvChad

---
+++ init.lua
@@ -0,0 +1,24 @@
+-- custom/init.lua from NvChad
+
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+local opt = vim.opt
+local new_cmd = vim.api.nvim_create_user_command
+
+-- Numbers
+opt.relativenumber = true
+
+-- Indenting 
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+
+new_cmd('Tex', function ()
+    require('nvterm.terminal').send('latexmk -auxdir=aux -pdf main.tex && evince main.pdf & latexmk -auxdir=aux -pdf -pvc main.tex')
+end, {})
