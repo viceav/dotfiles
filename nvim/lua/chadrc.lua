0af66dc9cd2b9a643ecb75ce8c0df72f0587794e
viceav <vicentealamosv@gmail.com>
2024-10-01 15:26:11 -0300

    refactor: update chadrc config

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -5,29 +5,25 @@ local M = {}
 local highlights = require "highlights"
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = {},
-
-  hl_override = highlights.override,
-  hl_add = highlights.add,
-
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      " ___      ___ ___  ________  _______      ",
-      "|\\  \\    /  /|\\  \\|\\   ____\\|\\  ___ \\     ",
-      "\\ \\  \\  /  / | \\  \\ \\  \\___|\\ \\   __/|    ",
-      " \\ \\  \\/  / / \\ \\  \\ \\  \\    \\ \\  \\_|/__  ",
-      "  \\ \\    / /   \\ \\  \\ \\  \\____\\ \\  \\_|\\ \\ ",
-      "   \\ \\__/ /     \\ \\__\\ \\_______\\ \\_______\\",
-      "    \\|__|/       \\|__|\\|_______|\\|_______|",
-      "                                          ",
-    },
-  },
-
   statusline = {
     theme = "vscode_colored",
     order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" },
+  },
+}
+
+M.base46 = { theme = "bearded-arc", theme_toggle = {}, hl_override = highlights.override, hl_add = highlights.add }
+
+M.nvdash = {
+  load_on_startup = true,
+  header = {
+    " ___      ___ ___  ________  _______      ",
+    "|\\  \\    /  /|\\  \\|\\   ____\\|\\  ___ \\     ",
+    "\\ \\  \\  /  / | \\  \\ \\  \\___|\\ \\   __/|    ",
+    " \\ \\  \\/  / / \\ \\  \\ \\  \\    \\ \\  \\_|/__  ",
+    "  \\ \\    / /   \\ \\  \\ \\  \\____\\ \\  \\_|\\ \\ ",
+    "   \\ \\__/ /     \\ \\__\\ \\_______\\ \\_______\\",
+    "    \\|__|/       \\|__|\\|_______|\\|_______|",
+    "                                          ",
   },
 }
 
