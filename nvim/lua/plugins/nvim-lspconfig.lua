f691e2f0d5be8ce0f464a92b05e14740d3823297
viceav <vicentealamosv@gmail.com>
2024-11-01 01:26:04 -0300

    feat(nvim): update LSP configurations and key mappings
    - Remove `lua_ls` from the list of LSP servers
    - Add key mappings for various LSP functionalities based on client capabilities
    - Attach key mappings to LSP clients on setup
    - Configure `lua_ls` with specific settings for LuaJIT and Neovim runtime files

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,52 @@
-local servers = { "html", "cssls", "ts_ls", "clangd", "pylsp", "jsonls", "bashls", "cmake", "lua_ls" }
+local servers = { "html", "cssls", "ts_ls", "clangd", "pylsp", "jsonls", "bashls", "cmake" }
+
+local map = vim.keymap.set
+
+local function set_mappings(client, bufnr)
+  local function opts(desc)
+    return { buffer = bufnr, desc = "LSP " .. desc }
+  end
+
+  if client.supports_method "textDocument/implementation" then
+    map("n", "gi", vim.lsp.buf.implementation, opts "Go to implementation")
+  end
+  if client.supports_method "textDocument/declaration" then
+    map("n", "gD", vim.lsp.buf.declaration, opts "Go to declaration")
+  end
+  if client.supports_method "textDocument/definition" then
+    map("n", "gd", vim.lsp.buf.definition, opts "Go to definition")
+  end
+  if client.supports_method "textDocument/typeDefinition" then
+    map("n", "<leader>D", vim.lsp.buf.type_definition, opts "Go to type definition")
+  end
+  if client.supports_method "textDocument/signatureHelp" then
+    map("n", "<leader>sh", vim.lsp.buf.signature_help, opts "Show signature help")
+  end
+  if client.supports_method "workspace/workspaceFolders" then
+    map("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts "Add workspace folder")
+    map("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts "Remove workspace folder")
+    map("n", "<leader>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts "List workspace folders")
+  end
+  if client.supports_method "textDocument/diagnostics" then
+    map("n", "<leader>we", function()
+      vim.diagnostic.setqflist { severity = "ERROR" }
+    end, opts "Show Errors")
+    map("n", "<leader>ww", function()
+      vim.diagnostic.setqflist { severity = "WARN" }
+    end, opts "Show Warnings")
+  end
+  if client.supports_method "textDocument/rename" then
+    map("n", "<leader>rn", vim.lsp.buf.rename, opts "Rename")
+  end
+  if client.supports_method "textDocument/codeAction" then
+    map("n", "<leader>ca", vim.lsp.buf.code_action, opts "Code Action")
+  end
+  if client.supports_method "textDocument/references" then
+    map("n", "gr", vim.lsp.buf.references, opts "Show references")
+  end
+end
 
 local M = {
   "neovim/nvim-lspconfig",
@@ -10,11 +58,17 @@ local M = {
     for _, lsp in ipairs(servers) do
       lspconfig[lsp].setup {
         capabilities = capabilities,
+        on_attach = function(client, bufnr)
+          set_mappings(client, bufnr)
+        end,
       }
     end
 
     lspconfig.texlab.setup {
       capabilities = capabilities,
+      on_attach = function(client, bufnr)
+        set_mappings(client, bufnr)
+      end,
       settings = {
         texlab = {
           build = {
@@ -33,8 +87,39 @@ local M = {
         },
       },
     }
-  end,
 
+    lspconfig.lua_ls.setup {
+      on_attach = function(client, bufnr)
+        set_mappings(client, bufnr)
+      end,
+      on_init = function(client)
+        if client.workspace_folders then
+          local path = client.workspace_folders[1].name
+          if vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc") then
+            return
+          end
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME,
+            },
+          },
+        })
+      end,
+      settings = {
+        Lua = {},
+      },
+    }
+  end,
   event = "BufRead",
 }
 
