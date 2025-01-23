12f00efc66bff30c5264403f7610c721b006c652
viceav <vicentealamosv@gmail.com>
2025-01-23 03:38:12 -0300

    feat(quickmenu): add revealer transition and remove gdkMonitor prop
    - Added a Gtk.Revealer with slide left transition and duration of 500ms to the QuickMenu component.
    - Removed the gdkMonitor prop from the QuickMenu component.
    - Updated the SCSS to reflect the new structure with the revealer.

--- ags/widget/quickmenu/Quickmenu.tsx
+++ ags/widget/quickmenu/Quickmenu.tsx
@@ -1,5 +1,5 @@
 import { bind } from "astal";
-import { App, Astal, Gdk, Gtk } from "astal/gtk3";
+import { App, Astal, Gtk } from "astal/gtk3";
 import AstalWp from "gi://AstalWp?version=0.1";
 import Brightness from "../../service/brightness";
 import AstalMpris from "gi://AstalMpris?version=0.1";
@@ -9,7 +9,7 @@ const brightness = Brightness.get_default();
 const spotify = AstalMpris.Player.new("spotify");
 const sliderWidth = 150;
 
-export default function QuickMenu(gdkMonitor: Gdk.Monitor) {
+export default function QuickMenu() {
   const { TOP, RIGHT } = Astal.WindowAnchor;
 
   return (
@@ -18,77 +18,87 @@ export default function QuickMenu(gdkMonitor: Gdk.Monitor) {
       visible={false}
       name={"QuickMenu"}
       className={"QuickMenu"}
-      gdkmonitor={gdkMonitor}
       anchor={TOP | RIGHT}
       application={App}
     >
-      <box vertical={true} spacing={5}>
-        <box spacing={20}>
-          <box
-            widthRequest={50}
-            heightRequest={50}
-            className={"grogu"}
-            valign={Gtk.Align.CENTER}
-            halign={Gtk.Align.CENTER}
-          ></box>
-          <box vertical={true} valign={Gtk.Align.CENTER} homogeneous={true}>
-            <box>
-              <label label={""}></label>
-              <slider
-                valign={Gtk.Align.CENTER}
-                halign={Gtk.Align.START}
-                onDragged={({ value }) => (wp.volume = value)}
-                value={bind(wp, "volume")}
-                widthRequest={sliderWidth}
-              ></slider>
+      <revealer
+        transitionType={Gtk.RevealerTransitionType.SLIDE_LEFT}
+        transitionDuration={500}
+        setup={(self) => {
+          self.hook(self, "map", () => (self.revealChild = true));
+          self.hook(self, "unmap", () => (self.revealChild = false));
+        }}
+      >
+        <box vertical={true} spacing={5}>
+          <box spacing={20}>
+            <box
+              widthRequest={50}
+              heightRequest={50}
+              className={"grogu"}
+              valign={Gtk.Align.CENTER}
+              halign={Gtk.Align.CENTER}
+            ></box>
+            <box vertical={true} valign={Gtk.Align.CENTER} homogeneous={true}>
+              <box>
+                <label label={""}></label>
+                <slider
+                  valign={Gtk.Align.CENTER}
+                  halign={Gtk.Align.START}
+                  onDragged={({ value }) => (wp.volume = value)}
+                  value={bind(wp, "volume")}
+                  widthRequest={sliderWidth}
+                ></slider>
+              </box>
+              <box>
+                <label label={"󰃠"}></label>
+                <slider
+                  valign={Gtk.Align.CENTER}
+                  halign={Gtk.Align.START}
+                  onDragged={({ value }) => (brightness.screen = value)}
+                  value={bind(brightness, "screen")}
+                  widthRequest={sliderWidth}
+                ></slider>
+              </box>
             </box>
-            <box>
-              <label label={"󰃠"}></label>
-              <slider
-                valign={Gtk.Align.CENTER}
+          </box>
+          <box spacing={10}>
+            <box
+              className={"cover"}
+              css={bind(spotify, "coverArt").as((coverArt) => {
+                if (coverArt) {
+                  return `background-image: url("${coverArt}")`;
+                } else {
+                  return "";
+                }
+              })}
+            ></box>
+            <box vertical={true} valign={Gtk.Align.CENTER}>
+              <label
+                maxWidthChars={25}
+                truncate={true}
+                justify={Gtk.Justification.LEFT}
                 halign={Gtk.Align.START}
-                onDragged={({ value }) => (brightness.screen = value)}
-                value={bind(brightness, "screen")}
-                widthRequest={sliderWidth}
-              ></slider>
+                valign={Gtk.Align.END}
+                label={bind(spotify, "title").as((title) =>
+                  title ? title : "",
+                )}
+                css={'font-family: "Sans"; font-size: 14px;'}
+              ></label>
+              <label
+                maxWidthChars={25}
+                truncate={true}
+                justify={Gtk.Justification.LEFT}
+                halign={Gtk.Align.START}
+                valign={Gtk.Align.START}
+                label={bind(spotify, "artist").as((artist) =>
+                  artist ? artist : "",
+                )}
+                css={'font-family: "Sans"; font-size: 12px; color: #a3a3a3; '}
+              ></label>
             </box>
           </box>
         </box>
-        <box spacing={10}>
-          <box
-            className={"cover"}
-            css={bind(spotify, "coverArt").as((coverArt) => {
-              if (coverArt) {
-                return `background-image: url("${coverArt}")`;
-              } else {
-                return "";
-              }
-            })}
-          ></box>
-          <box vertical={true} valign={Gtk.Align.CENTER}>
-            <label
-              maxWidthChars={25}
-              truncate={true}
-              justify={Gtk.Justification.LEFT}
-              halign={Gtk.Align.START}
-              valign={Gtk.Align.END}
-              label={bind(spotify, "title").as((title) => (title ? title : ""))}
-              css={'font-family: "Sans"; font-size: 14px;'}
-            ></label>
-            <label
-              maxWidthChars={25}
-              truncate={true}
-              justify={Gtk.Justification.LEFT}
-              halign={Gtk.Align.START}
-              valign={Gtk.Align.START}
-              label={bind(spotify, "artist").as((artist) =>
-                artist ? artist : "",
-              )}
-              css={'font-family: "Sans"; font-size: 12px; color: #a3a3a3; '}
-            ></label>
-          </box>
-        </box>
-      </box>
+      </revealer>
     </window>
   );
 }
