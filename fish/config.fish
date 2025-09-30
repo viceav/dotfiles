8903ff986e1b85db0c3431b9dfea85aa773a01aa
viceav <vicentealamosv@gmail.com>
2025-09-29 22:11:49 -0300

    feat: update hyprlock and niri configs

--- fish/config.fish
+++ fish/config.fish
@@ -6,13 +6,14 @@ zoxide init fish | source
 
 alias pacinfo "pacman -Qei | rg -U 'Name|Description|^\n'"
 
-set -x XCURSOR_PATH $HOME/.local/share/icons
-set -x EDITOR nvim
-
 set -x fish_greeting
 
 if status is-login
     if test (tty) = /dev/tty1
+        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+        set -x XCURSOR_PATH $HOME/.local/share/icons
+        set -x EDITOR nvim
+
         brightnessctl set 100%
         # A copy of niri-session without the SHELL part,
         # Using the original causes a loop
